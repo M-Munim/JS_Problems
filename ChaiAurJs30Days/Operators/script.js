47999f52db19afe9adb61de50dccd842d65a6a9d
munim <muhammadmunimoff330@gmail.com>
2024-07-17 21:17:35 +0500

    ChaiAurJs30Days Q2

---
+++ ChaiAurJs30Days/Operators/script.js
@@ -0,0 +1,78 @@
+// ------------------------------- Activity 1
+/*
+let NumOne = 404;
+let NumTwo = 400;
+console.log(NumOne + NumTwo);
+
+let NumOnee = 404;
+let NumTwoo = 400;
+console.log(NumOne - NumTwo);
+
+let NummOne = 404;
+let NummTwo = 400;
+console.log(NumOne / NumTwo);
+
+let NuumOne = 404;
+let NuumTwo = 400;
+console.log(NumOne * NumTwo);
+
+let NumOnne = 404;
+let NumTwwo = 400;
+console.log(NumOne % NumTwo);
+*/
+
+// ------------------------------- Activity 2
+/*
+let amount = 10;
+amount += 20;
+console.log(amount);
+
+let cash = 150;
+cash -= 100;
+console.log(cash);
+*/
+
+// ------------------------------- Activity 3
+/*
+let first = 400;
+let second = 300;
+
+console.log(first < second);
+console.log(first > second);
+console.log(first >= second);
+console.log(first <= second);
+
+let myAge = 33;
+let YourAge = "33";
+
+console.log(myAge == YourAge);
+console.log(myAge === YourAge);
+*/
+
+// ------------------------------- Activity 4
+// let age = 30;
+// let IsEmployee = false;
+
+// let canEnter = age >= 18 && IsEmployee;
+// console.log("Can Enter :", canEnter);
+
+// // -----------------------
+// let hasCsDegree = false;
+// let IsEmployee = true;
+
+// let canEnter = hasCsDegree || IsEmployee;
+// console.log("Can Enter :", canEnter);
+
+// // -----------------------
+// let age = 30;
+// let IsEmployee = false;
+
+// let canEnter = age >= 18 && !IsEmployee;
+// console.log("Can Enter :", canEnter);
+
+// ------------------------------- Activity 5
+let NumOne = 33;
+let NumTwo = -3;
+
+let result = NumOne > NumTwo ? true : false;
+console.log(result);
