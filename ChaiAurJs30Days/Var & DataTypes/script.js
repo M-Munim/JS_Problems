e045f20a026728b3b86f5eb63565ab1d6c1527c5
munim <muhammadmunimoff330@gmail.com>
2024-07-17 20:37:43 +0500

    ChaiAurJs30Days Q1

---
+++ ChaiAurJs30Days/Var & DataTypes/script.js
@@ -0,0 +1,55 @@
+// ----------------------- Task NO 1
+/*
+var aVar = 100;
+console.log(aVar);
+*/
+
+// ----------------------- Task NO 1.1
+/*
+let myName = "munim";
+console.log(myName);
+*/
+
+// ----------------------- Task NO 2
+/*
+const isPresent = true;
+console.log(isPresent);
+*/
+
+// ----------------------- Task NO 3
+/*
+let myRollNum = 24;
+console.log(typeof myRollNum);
+
+let myFullName = "Muhammad Munim";
+console.log(typeof myFullName);
+
+let isAvailable = false;
+console.log(typeof isAvailable);
+
+const myDetails = {
+  id: "01",
+  name: "Munim",
+  domain: "Web Dev"
+}
+console.log(typeof myDetails);
+
+const rollNums = [223, 445, 31, 456, 664, 33];
+console.log(typeof rollNums);
+*/
+
+// ----------------------- Task NO 4
+/*
+let myInitialVal = 34;
+console.log(`${myInitialVal} is my Initial Val`);
+
+myInitialVal = 40;
+console.log(`${myInitialVal} is my Reassigned Val`);
+*/
+
+// ----------------------- Task NO 5
+const myInitialVal = 6900;
+console.log(`${myInitialVal} is my Initial Val`);
+
+myInitialVal = 4000;
+console.log(`${myInitialVal} is my Reassigned Val`);
